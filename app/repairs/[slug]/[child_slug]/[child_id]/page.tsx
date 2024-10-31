03dbf7a9b7e439eaf664afc7dd65d2f50448efdb
techwecaregr <techwecaregr@gmail.com>
2024-10-31 16:59:02 +0200

    import pageprops

--- app/repairs/[slug]/[child_slug]/[child_id]/page.tsx
+++ app/repairs/[slug]/[child_slug]/[child_id]/page.tsx
@@ -1,10 +1,11 @@
 import ChildIDPage from '@/components/pages/ChildIDPage';
+import PageProps from 'next/app';
 interface Params {
   slug?: string;
   child_slug?: string;
   child_id?: string;
 }
-const SingleProductPage = ({ params }: {params:Params})  => {
+const SingleProductPage = ({ params }: PageProps & {params:Params})  => {
   return <ChildIDPage params= {params}/>
 }
 
